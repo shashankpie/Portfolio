f7a7333a11618b852f9d20e48a32a8ded9da3ac5
Shiva Shashank <shashanktech99@gmail.com>
2022-02-25 14:04:28 -0600

    quick update

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <h1 className="font-IBM m-4">Hello world, welcome to my portfolio</h1>;
+  return <h1 className="font-IBM m-4">This Portfolio is under development, upgrading to Next.js 12. Stay Tuned!</h1>;
 }
