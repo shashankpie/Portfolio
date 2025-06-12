7e04177585bc3aa7774e0780fd357f2de40c4e10
Shiva Shashank <shashanktech99@gmail.com>
2025-06-12 16:12:05 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+My Personal website built with Next.js, Tailwind CSS and deployed to Vercel.
---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <>
+      <h1>Cooking...stay tuned!</h1>
+    </>
+  );
+}
